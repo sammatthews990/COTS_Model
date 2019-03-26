749ea7a00849c65c7069056d1971b53c9c382477
sammatthews990 <sammatthews990@gmail.com>
2019-03-25 17:18:14 -0700

    Created new model script for HPC

---
+++ COTSMod.R
@@ -0,0 +1,32 @@
+##############
+# COTSMod: Spatially Explicit COTS-Coral Metapopulation Model ----
+##############
+
+
+# Load Pre Defined Data ----
+
+load(...)
+
+# Load Functions ----
+
+source(...)
+
+# Set Global Parameters ----
+
+
+# Resample Disturbance ----
+
+
+# Save Workspace for import within each thread ----
+
+
+
+# PARALLEL
+
+
+load(...)
+
+
+
+
+
